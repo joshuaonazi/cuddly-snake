7c06dfdabfdcc74c8b6bd9778bf60e503401940b
Vishwesh Gaur <52960742+Vish958@users.noreply.github.com>
2019-08-24 15:12:22 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,75 +1,83 @@
-from string import ascii_lowercase
-from words import get_random_word
- 
- 
-def get_num_attempts():
-    """Get user-inputted number of incorrect attempts for the game."""
-    while True:
-        num_attempts = input(
-            'How many incorrect attempts do you want? [1-25] ')
-        try:
-            num_attempts = int(num_attempts)
-            if 1 <= num_attempts <= 25:
-                return num_attempts
-            else:
-                print('{0} is not between 1 and 25'.format(num_attempts))
-        except ValueError:
-            print('{0} is not an integer between 1 and 25'.format(
-                num_attempts))
- 
- 
-def get_min_word_length():
-    """Get user-inputted minimum word length for the game."""
-    while True:
-        min_word_length = input(
-            'What minimum word length do you want? [4-16] ')
-        try:
-            min_word_length = int(min_word_length)
-            if 4 <= min_word_length <= 16:                 return min_word_length             else:                 print('{0} is not between 4 and 16'.format(min_word_length))         except ValueError:             print('{0} is not an integer between 4 and 16'.format(                 min_word_length)) def get_display_word(word, idxs):     """Get the word suitable for display."""     if len(word) != len(idxs):         raise ValueError('Word length and indices length are not the same')     displayed_word = ''.join(         [letter if idxs[i] else '*' for i, letter in enumerate(word)])     return displayed_word.strip() def get_next_letter(remaining_letters):     """Get the user-inputted next letter."""     if len(remaining_letters) == 0:         raise ValueError('There are no remaining letters')     while True:         next_letter = input('Choose the next letter: ').lower()         if len(next_letter) != 1:             print('{0} is not a single character'.format(next_letter))         elif next_letter not in ascii_lowercase:             print('{0} is not a letter'.format(next_letter))         elif next_letter not in remaining_letters:             print('{0} has been guessed before'.format(next_letter))         else:             remaining_letters.remove(next_letter)             return next_letter def play_hangman():     """Play a game of hangman.     At the end of the game, returns if the player wants to retry.     """     # Let player specify difficulty     print('Starting a game of Hangman...')     attempts_remaining = get_num_attempts()     min_word_length = get_min_word_length()     # Randomly select a word     print('Selecting a word...')     word = get_random_word(min_word_length)     print()     # Initialize game state variables     idxs = [letter not in ascii_lowercase for letter in word]     remaining_letters = set(ascii_lowercase)     wrong_letters = []     word_solved = False     # Main game loop     while attempts_remaining > 0 and not word_solved:
-        # Print current game state
-        print('Word: {0}'.format(get_display_word(word, idxs)))
-        print('Attempts Remaining: {0}'.format(attempts_remaining))
-        print('Previous Guesses: {0}'.format(' '.join(wrong_letters)))
- 
-        # Get player's next letter guess
-        next_letter = get_next_letter(remaining_letters)
- 
-        # Check if letter guess is in word
-        if next_letter in word:
-            # Guessed correctly
-            print('{0} is in the word!'.format(next_letter))
- 
-            # Reveal matching letters
-            for i in range(len(word)):
-                if word[i] == next_letter:
-                    idxs[i] = True
+#importing the time module
+import time
+
+#welcoming the user
+name = input("What is your name? ")
+
+print ("Hello, " + name, "Time to play hangman!")
+
+
+#wait for 1 second
+time.sleep(1)
+
+print ("Start guessing...")
+time.sleep(0.5)
+
+#here we set the secret
+word = "secret"
+
+#creates an variable with an empty value
+guesses = ''
+
+#determine the number of turns
+turns = 10
+
+# Create a while loop
+
+#check if the turns are more than zero
+while turns > 0:         
+
+    # make a counter that starts with zero
+    failed = 0             
+
+    # for every character in secret_word    
+    for char in word:      
+
+    # see if the character is in the players guess
+        if char in guesses:    
+    
+        # print then out the character
+            print ("char",)    
+
         else:
-            # Guessed incorrectly
-            print('{0} is NOT in the word!'.format(next_letter))
+    
+        # if not found, print a dash
+            print ("_",)     
+       
+        # and increase the failed counter with one
+            failed += 1    
+
+    # if failed is equal to zero
+
+    # print You Won
+    if failed == 0:        
+        print ("You won")  
+
+    # exit the script
+        break;  
+
+    print
+
+    # ask the user go guess a character
+    guess = input("guess a character:") 
+
+    # set the players guess to guesses
+    guesses += guess                    
+
+    # if the guess is not found in the secret word
+    if guess not in word:  
  
-            # Decrement num of attempts left and append guess to wrong guesses
-            attempts_remaining -= 1
-            wrong_letters.append(next_letter)
+     # turns counter decreases with 1 (now 9)
+        turns -= 1        
  
-        # Check if word is completely solved
-        if False not in idxs:
-            word_solved = True
-        print()
+    # print wrong
+        print ("Wrong")    
  
-    # The game is over: reveal the word
-    print('The word is {0}'.format(word))
+    # how many turns are left
+        print ("You have"), + turns, ('more guesses') 
  
-    # Notify player of victory or defeat
-    if word_solved:
-        print('Congratulations! You won!')
-    else:
-        print('Try again next time!')
- 
-    # Ask player if he/she wants to try again
-    try_again = input('Would you like to try again? [y/Y] ')
-    return try_again.lower() == 'y'
- 
- 
-if __name__ == '__main__':
-    while play_hangman():
-        print()
+    # if the turns are equal to zero
+        if turns == 0:           
+    
+        # print "You Loose"
+            print ("You Loose") 
